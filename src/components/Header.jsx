8170135f6ef8f29201f6a6e6d2ffad2f894e3dff
Divyansh Jain <124493395+divyanshdj@users.noreply.github.com>
2024-10-24 21:34:26 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,16 @@
+import Navbar from "./Navbar";
+
 const Header = () => {
-    return(
-        <div className="h-16 bg-blue-950 flex justify-center items-center">
-            <h1 className="font-semibold text-white text-3xl">RTU SGPA CALCULATOR</h1>
-        </div>
-    );
+  return (
+    <>
+      <div className="h-16 bg-blue-950 flex justify-center items-center">
+        <h1 className="font-semibold text-white text-3xl">
+          RTU SGPA CALCULATOR
+        </h1>
+      </div>
+      <Navbar />
+    </>
+  );
 };
 
 export default Header;
